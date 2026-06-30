47f3d5d76603e78baebf86f6c83ffc4374be96a7
Vbubols <vitorbubols@gmail.com>
2026-06-29 21:59:15 -0300

    fixed typo

--- login_backend/src/schemas/auth.schema.js
+++ login_backend/src/schemas/auth.schema.js
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const cadastroSchema = z.object({
     nome: z.string().min(2, 'Nome deve ter pelo menos 2 caracteres'),
     email: z.string().email('E-mail inválido'),
-    senha: z.string().min(4, 'Senha deve ter pelo menos 6 caracteres')
+    senha: z.string().min(4, 'Senha deve ter pelo menos 4 caracteres')
 });
 
 export const loginSchema = z.object({
